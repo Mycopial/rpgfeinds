9bf10b8bfe358789700e4a0fe7d999dcec39dfb2
mycopial <59245026+Mycopial@users.noreply.github.com>
2025-11-21 10:49:11 -0500

    Antigravity website creation

---
+++ js/data.js
@@ -0,0 +1,12 @@
+// --- D&D BEYOND INTEGRATION CONFIG ---
+const PARTY_IDS = ['66469341', '10217862', '8679435', '11474097'];
+const PROXY_URL = "https://corsproxy.io/?";
+const foundryUrl = "https://foundryvtt.rpgfiends.com";
+
+// Pre-calculated 3D targets for the eye to look at (Random locations on planet)
+const TARGET_COORDS = [
+    { x: 1.5, y: 1.5, z: 1.5 },
+    { x: -1.5, y: -1.5, z: 1.0 },
+    { x: 0, y: 2.5, z: 0.5 },
+    { x: 2.0, y: 0, z: -1.0 }
+]; 
